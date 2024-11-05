4bd808f558d1929f20e2248d2f6419297eaf1643
LucasK <Lucas.kuntz@afpa-dev-pompey.fr>
2024-11-05 11:50:38 +0100

    upgrade TP3

--- PersonneAdresse/src/utilitaires/Outils.java
+++ PersonneAdresse/src/utilitaires/Outils.java
@@ -12,25 +12,32 @@ public class Outils {
 
     /**
      * Méthode d'affichage d'une personne.
+     *
      * @param titre Titre de la fenêtre de dialogue.
-     * @param p Personne à afficher.
+     * @param p     Personne à afficher.
      */
     static public void afficher(String titre, Personne p) {
+        // Affichage de la personne dans une fenêtre de dialogue.
         JOptionPane.showMessageDialog(null, p, titre, JOptionPane.INFORMATION_MESSAGE);
     }
 
     /**
      * Méthode d'affichage de plusieurs personnes
+     *
      * @param titre Titre de la fenêtre de dialogue.
-     * @param p Liste de personnes à afficher.
+     * @param p     Liste de personnes à afficher.
      */
     static public void afficher(String titre, ArrayList<Personne> p) {
+        // Initialisation de la variable stringbuilder.
         StringBuilder infos = new StringBuilder();
 
         for (Personne personne : p) {
+            // Pour chaque personne, ajout de celle ci sous forme de chaine
+            // de caractères.
             infos.append(personne.toString()).append("\n");
         }
 
+        // Affichage des personnes dans une fenêtre de dialogue.
         JOptionPane.showMessageDialog(null, infos.toString(), titre,
                 JOptionPane.INFORMATION_MESSAGE);
     }
